8dfa78a04bbfe1a8b7c69adf9a30cb93967e5aca
刘景平 <844869663@qq.com>
2021-08-23 18:22:30 +0800

    修改 Dubbo 3.0.2(Telnet 不支持 ls、invoke 等命令)为 3.0.0 版本; 添加 Dubbo 3 消费者示例; Dubbo 3 向注册中心注册应用级别的 URL 是 `/services/${applicarion-name}/${host}:${post}` 形式的节点，该节点数据保存了这个应用的相关信息

---
+++ dubbo3-demo-consumer/src/main/java/org/apache/dubbo/demo/service/impl/StudyDubboServiceImpl.java
@@ -0,0 +1,37 @@
+package org.apache.dubbo.demo.service.impl;
+
+import org.apache.dubbo.config.annotation.DubboReference;
+import org.apache.dubbo.config.annotation.DubboService;
+import org.apache.dubbo.demo.model.ResearchResult;
+import org.apache.dubbo.demo.model.Scholar;
+import org.apache.dubbo.demo.service.StudyDubboService;
+import org.apache.dubbo.demo.service.StudyService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+/**
+ * @author jingping.liu
+ * @date 2021-08-23
+ * @description
+ */
+@DubboService(version = "2.0.0")
+public class StudyDubboServiceImpl implements StudyDubboService {
+
+    /**
+     * 直接通过 {@link DubboReference} 注解注入远程服务（消费）
+     * 如果这里使用 {@link Autowired} 注解，
+     * 则需要在配置文件里面通过 `<dubbo:reference />` 进行配置
+     */
+    @DubboReference(version = "2.0.0")
+    StudyService studyService;
+
+    @Override
+    public String studyDubbo(Scholar scholar) {
+        ResearchResult result = studyService.researchDubbo(scholar);
+        if (result != null) {
+            System.out.println("学习 Dubbo3 结果：" + result.toString());
+        } else {
+            System.out.println("学习失败");
+        }
+        return "study...";
+    }
+}
